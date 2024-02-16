e4f1998f16e89ad70c7e8298c013c1069fc5b125
sumit <sumitme00117@gmail.com>
2024-02-16 16:34:21 +0530

    bug fixes

--- src/components/Login.js
+++ src/components/Login.js
@@ -75,7 +75,7 @@ const Login = () => {
     <div>
       <Header/>
       <div className='absolute'>
-      <img className='h-screen object-cover' src={BG_URL} alt="" />
+      <img className='h-screen object-cover md:w-screen' src={BG_URL} alt="" />
       </div>
       <form onSubmit={(e) => e.preventDefault()} className='w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
         <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
